f7dbda09d060d5f89447c360ca672f7b3e00fbfc
Erick <erick.nahuelpaiva.001@gmail.com>
2022-09-29 11:11:05 -0300

    mejoras a la pagina

---
+++ appDesayunos.js
@@ -0,0 +1,44 @@
+import { Desayuno } from "./Desayuno.js";
+import { Tarjeta } from "./Tarjeta.js";
+
+const unDesayuno = new Desayuno("Café", "cortado", "https://statics-cuidateplus.marca.com/cms/images/cafe-salud.jpg");
+
+const unaTarjeta = new Tarjeta(
+    unDesayuno.getNombre(),
+    unDesayuno.getTipoPreparacion(),
+    unDesayuno.getFoto()
+);
+
+const vectorTarjetas = [unaTarjeta,otraTarjeta,tresTarjeta,cuatroTarjeta,unaTarjeta]
+
+let tarjetasGeneradas = ``;
+
+for (let i = 0; i < 10; i++) {
+   tarjetasGeneradas += `
+    <div class="col">
+        ${unaTarjeta.generarHtml()}
+    </div>
+   `
+}
+
+document.querySelector("#contenedor-cards").innerHTML =`
+    <div class="row">
+        ${tarjetasGeneradas}
+    </div>
+`;
+
+
+
+/* function agregarTarjeta(){
+    document.querySelector("#contenedor-cards").innerHTML +=`
+    <div class="row">
+        <div class="col">
+            ${unaTarjeta.generarHtml()}
+        </div>
+    </div>
+    `;
+}
+
+for (let i = 0; i < 10; i++) {
+    agregarTarjeta();
+} */
